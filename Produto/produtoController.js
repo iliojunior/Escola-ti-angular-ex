62d6538098277a8fe9aeb47b3074d2b8045949c5
Ilio Junior <ilioadriano@live.com>
2017-03-16 22:41:54 -0300

    Adicionando e excluindo produtos

--- Produto/produtoController.js
+++ Produto/produtoController.js
@@ -1,3 +1,48 @@
-/**
- * Created by souzaeduardoac on 16/03/17.
- */
+(function () {
+
+    'use strict';
+
+    var app = angular
+        .module('app');
+
+    app.controller('ProdutosController', ProdutosController);
+
+    var produtosTeste = [
+        {
+            codigo: 1,
+            nome: '1',
+            preco: 1,
+            estoque: 1
+        },
+        {
+            codigo: 2,
+            nome: '2',
+            preco: 2,
+            estoque: 2
+        },
+    ];
+
+    var produtos = produtosTeste;
+
+    function ProdutosController() {
+        this.products = produtos;
+        this.produtoEditavel = {};
+
+        this.save = function () {
+            this.products.push(this.produtoEditavel);
+            this.produtoEditavel = {};
+        };
+
+        this.edit = function (produto) {
+            this.produtoEditavel = produto;
+        };
+
+        this.delete = function (produto) {
+            produtos = produtos.filter(function (element) {
+                return (element !== produto);
+            });
+            this.products = produtos;
+        };
+    }
+
+})();
--- application/app.js
+++ application/app.js
@@ -2,30 +2,27 @@
 
     'use strict';
 
-    var app = angular.module('app', []);
+    var app = angular
+        .module('app');
 
-    app.controller('TabController', TabController);
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    function config($routeProvider, $locationProvider) {
 
-    app.directive('productTab', productTab);
+        $routeProvider
+            .when('/produtos', {
+                controller: 'ProdutosController',
+                templateUrl: 'Produto/produto.html',
+                controllerAs: 'ProdutosCtrl'
+            })
+            .when('/pedidos', {
+                controller: 'PedidosController',
+                templateUrl: 'Pedido/pedido.html'
+            })
+            .otherwise({redirectTo: '/produtos'});
 
-    function productTab() {
-        return {
-            restrict: 'E',
-            templateUrl: 'Produto/produto.html'
-        };
     }
 
-    function TabController() {
 
-        this.tab = 1;
-
-        this.setTab = function (tab) {
-            this.tab = tab;
-        };
-
-        this.isSelected = function (tab) {
-            return (this.tab === tab);
-        };
-    }
+    app.config(config);
 
 })();
