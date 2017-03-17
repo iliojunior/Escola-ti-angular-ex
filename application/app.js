7aabe2b74a3cd8ec5bda0557558d1d0c6ce20a8c
Ilio Junior <ilioadriano@live.com>
2017-03-16 21:24:27 -0300

    ASD

--- application/app.js
+++ application/app.js
@@ -4,4 +4,28 @@
 
     var app = angular.module('app', []);
 
+    app.controller('TabController', TabController);
+
+    app.directive('productTab', productTab);
+
+    function productTab() {
+        return {
+            restrict: 'E',
+            templateUrl: 'Produto/produto.html'
+        };
+    }
+
+    function TabController() {
+
+        this.tab = 1;
+
+        this.setTab = function (tab) {
+            this.tab = tab;
+        };
+
+        this.isSelected = function (tab) {
+            return (this.tab === tab);
+        };
+    }
+
 })();
